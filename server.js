acb19faca8e8a2c1a473d89d30641b0bc2e9ac05
Trumpold <bltrumpold@labs.neit.edu>
2020-02-24 19:08:36 -0500

    herotest

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const hbs =  require('hbs')
+const app = express()
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({extended:false}))
+
+hbs.registerHelper('ptag', (num, words)=>{
+    var str = ''
+    for(let i=0; i < num; i++)
+    {
+        str+='<p>'
+        str+=words
+        str+='</p>'
+    }
+    
+    return new hbs.handlebars.SafeString(str)
+})
+
+function rando(req, res, next)
+{
+    req.num = Math.round(Math.random()*25)
+    next()
+}
+
+app.use(rando)
+
+app.get('/', (req, res)=>{
+    res.render('index', {name:"brandon's"})
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is running at localhost:3000')
+})
